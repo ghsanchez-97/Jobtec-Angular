6782f265e42c51f13b37bcf70378d2270cb6180f
Guy <anakin25@live.com>
2020-11-02 10:23:43 -0600

    Habilitar

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaders, HttpResponse } from '@angular/common/http';
-// import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { global } from './global';
 @Injectable({
